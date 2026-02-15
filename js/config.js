fa9b7d26ea3ee7ba2abb5786db856f533b6c3557
idantler <pirlxgamer@gmail.com>
2026-02-15 09:23:42 -0400

    Add EmailJS config for GitHub Pages

---
+++ js/config.js
@@ -0,0 +1,9 @@
+const EMAIL_CONFIG = {
+  serviceId: 'service_rowpp9j',
+  templateId: 'template_8jcbild',
+  publicKey: 'AE5Ol3cUljRG0Eja1'
+};
+
+if (typeof window !== 'undefined') {
+  window.EMAIL_CONFIG = EMAIL_CONFIG;
+}
